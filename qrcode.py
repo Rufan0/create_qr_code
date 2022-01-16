773516860ef186440ce58602f9110bca85c6efbf
Rufan <86017408+Rufan0@users.noreply.github.com>
2022-01-16 13:18:22 +0400

    Update and rename README.md to qrcode.py

---
+++ qrcode.py
@@ -0,0 +1,21 @@
+#Python-la istədiyiniz linkin QR kodunu hazırlaya bilərsiniz
+
+
+![python foto](https://i.imgur.com/9naUJpb.png)
+
+import qrcode
+
+#linki daxil edin
+link = "https://github.com/Rufan0"
+
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(link)
+
+qr.make(fit=True)
+
+img = qr.make_image(fill='black', back_color='white')
+img.save('rufan.png')
+print("Qr Code yaradıldı ...")
